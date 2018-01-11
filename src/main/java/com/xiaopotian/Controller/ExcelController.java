ad6f274f54f06bde558a4e3facaf36c5b848247c
BobSimon <34537967+BobSimon@users.noreply.github.com>
2018-01-11 17:07:36 +0800

    Update ExcelController.java
    添加一些提示

--- src/main/java/com/xiaopotian/Controller/ExcelController.java
+++ src/main/java/com/xiaopotian/Controller/ExcelController.java
@@ -8,20 +8,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by zouLu on 2017-12-14.
+ * 导出Excel控制层
+ *
+ * @Author  Bob Simon
+ * @Date 2018-01-08 17:45
  */
 @RestController
 public class ExcelController {
     @RequestMapping(value = "/excel", method = RequestMethod.GET)
     public void excel(HttpServletResponse response) throws Exception {
         ExcelData data = new ExcelData();
+        /*
+          需要特别注意的是：
+            这里的titles的size()要和每个row的size()要一样；
+            在这里titles和row指的该方法的List
+        */
+        
+        /**
+         * excel页标签名
+         */
         data.setName("hello");
+        
+        /**
+         * excel标题
+         */
         List<String> titles = new ArrayList();
         titles.add("a1");
         titles.add("a2");
         titles.add("a3");
         data.setTitles(titles);
 
+        /**
+         * excel对应每列的数据
+         */
         List<List<Object>> rows = new ArrayList();
         List<Object> row = new ArrayList();
         row.add("11111111111");
